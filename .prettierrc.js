18d90bf6b1877d8df2ea29b676c7b1b8cc4a1dce
JosiahFu <josiahfu@gmail.com>
2023-11-28 10:07:34 -0800

    Add Prettier for formatting

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    tabWidth: 4,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+    singleAttributePerLine: false,
+    plugins: ['prettier-plugin-tailwindcss']
+}
