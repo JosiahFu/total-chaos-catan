2722fb276d62e6472f8b6222281319289ba8ffaf
JosiahFu <josiahfu@gmail.com>
2023-11-30 15:44:30 -0800

    Store settings in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,29 @@ import { useState } from 'react';
 import { PhaseRecord } from './lib/data';
 import Arbiter from './components/Arbiter';
 import Settings from './components/Settings';
+import { useLocalStorage } from './lib/useLocalstorage';
 
 function App() {
-    const [players, setPlayers] = useState([
-        'Player 1',
-        'Player 2',
-        'Player 3',
-        'Player 4',
-    ]);
-    const [phaseLengths, setPhaseLengths] = useState<PhaseRecord<number>>({
-        resource: 20,
-        robber1: 20,
-        robber2: 20,
-        build_trade: 60,
-        cooldown: 20,
-    });
-    const [soundsEnabled, setSoundsEnabled] = useState(false);
+    const [players, setPlayers] = useLocalStorage(
+        ['Player 1', 'Player 2', 'Player 3', 'Player 4'],
+        'players'
+    );
+    const [phaseLengths, setPhaseLengths] = useLocalStorage<
+        PhaseRecord<number>
+    >(
+        {
+            resource: 20,
+            robber1: 20,
+            robber2: 20,
+            build_trade: 60,
+            cooldown: 20,
+        },
+        'phaseLengths'
+    );
+    const [soundsEnabled, setSoundsEnabled] = useLocalStorage(
+        false,
+        'soundsEnabled'
+    );
 
     const [playing, setPlaying] = useState(false);
     const [bgClassName, setBgClassName] = useState('');
---
+++ src/lib/useLocalstorage.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState, Dispatch, SetStateAction } from 'react';
+
+function useLocalStorage<T>(initialState: T | (() => T), key: string): [T, Dispatch<SetStateAction<T>>] {
+    const [state, setState] = useState<T>(() => JSON.parse(localStorage.getItem(key) ?? 'null') ?? initialState);
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state));
+    }, [key, state]);
+
+    return [state, setState];
+}
+
+export { useLocalStorage };
